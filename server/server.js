c20e1a9040ebf96804ab8394f34acd7f2274036c
pranjalkuhikar <pranjalkuhikar123@gmail.com>
2026-04-24 17:33:34 +0530

    chore(server): add start script and server entry point
    - Add "start" script to package.json to run server.js
    - Create server.js as the main entry point that imports app and config

---
+++ server/server.js
@@ -0,0 +1,8 @@
+import app from "./src/app.js";
+import config from "./src/config/config.js";
+
+const port = config.PORT;
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port} 🚀`);
+});
