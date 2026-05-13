e813622e49a5043b7bd830057dee4b0fda2ef976
pranjalkuhikar <pranjalkuhikar123@gmail.com>
2026-05-13 20:34:02 +0530

    feat(components): add barrel export file for components

---
+++ client/components/index.ts
@@ -0,0 +1,6 @@
+export { Header } from "./Header";
+export { Sidebar } from "./Sidebar";
+export { FileUpload } from "./FileUpload";
+export { URLAnalyzer } from "./URLAnalyzer";
+export { MessageList } from "./MessageList";
+export { ChatInput } from "./ChatInput";
